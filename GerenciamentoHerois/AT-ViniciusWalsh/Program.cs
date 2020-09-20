fd1398b2ac36ea8759c96b02aa87c9efa8689804
Walsh <vmw.walsh@gmail.com>
2020-09-20 20:24:11 -0300

    AT Final

---
+++ GerenciamentoHerois/AT-ViniciusWalsh/Program.cs
@@ -0,0 +1,37 @@
+﻿using Dominio;
+using Infraestrutura;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace WorkerService
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    var configRepositorio = hostContext.Configuration.GetValue<string>("AppSettings:configRepositorio");
+
+                    services.AddSingleton<IHeroiRepositorio>(provider => DefinirRepositorioInstancia(configRepositorio));
+                    services.AddHostedService<Worker>();
+                });
+
+        private static IHeroiRepositorio DefinirRepositorioInstancia(string configRepositorio)
+        {
+            if (configRepositorio == "AmigoRepositorioLinkedList")
+                return new HeroiRepositorioLinkedList();
+            else if (configRepositorio == "AmigoRepositorioList")
+                return new HeroiRepositorioList();
+            else
+                throw new NotImplementedException("Não existe implementação de repositório para configuração existente.");
+        }
+    }
+}
---
+++ GerenciamentoHerois/Dominio/IHeroiRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio
+{
+    public interface IHeroiRepositorio
+    {
+        IList<Heroi> Pesquisar(string termoDePesquisa);
+        void Adicionar(Heroi entidade);
+    }
+}
